d046e9f775100c71ffd156517d1ee0cf69e29e02
Maxim <incentivedie@gmail.com>
2024-08-14 17:04:12 +0400

    Add app metrica

--- FlexFit/Core/FlexFitApp.swift
+++ FlexFit/Core/FlexFitApp.swift
@@ -1,7 +1,23 @@
 import SwiftUI
+import AppMetricaCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        if let configuration = AppMetricaConfiguration(apiKey: "13d6ea88-d134-4c3f-abfe-f8f65362f039") {
+            configuration.locationTracking = false
+            AppMetrica.activate(with: configuration)
+        }
+        
+        return true
+    }
+}
 
 @main
 struct FlexFitApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     let persistenceController = PersistenceController.shared
     
@@ -11,7 +27,6 @@ struct FlexFitApp: App {
         WindowGroup {
             SplashScreen(showOnboarding: $showOnboarding, persistenceController: persistenceController)
                            .environment(\.managedObjectContext, persistenceController.container.viewContext)
-//                           .environmentObject(WebViewModel(appDelegate: appDelegate))
                            
         }
     }
--- FlexFit/Screens/SettingsScreen/SettingsScreen.swift
+++ FlexFit/Screens/SettingsScreen/SettingsScreen.swift
@@ -4,15 +4,21 @@ struct Settings: Identifiable {
     let id = UUID().uuidString
     let image: String
     let title: String
+    let url: String
 }
 
 struct SettingsScreen: View {
     
     private let settings: [Settings] = [
-        Settings(image: "bubble.fill", title: "Contact us"),
-        Settings(image: "wallet.pass.fill", title: "License"),
-        Settings(image: "menucard.fill", title: "Terms of use"),
-        Settings(image: "shield.fill", title: "Privacy")
+        Settings(image: "bubble.fill", 
+                 title: "Contact us",
+                 url: "https://www.termsfeed.com/live/0462b8bc-40c7-4b53-867a-3cd2286eb9c0"),
+        Settings(image: "menucard.fill",
+                 title: "Terms of use",
+                 url: "https://www.termsfeed.com/live/0462b8bc-40c7-4b53-867a-3cd2286eb9c0"),
+        Settings(image: "shield.fill",
+                 title: "Privacy",
+                 url: "https://www.termsfeed.com/live/13895a26-3e51-4a77-ad80-c8215a72311d")
     ]
     
     var body: some View {
@@ -24,22 +30,24 @@ struct SettingsScreen: View {
             ScrollView(showsIndicators: false) {
                 VStack(alignment: .leading) {
                     ForEach(settings) { setting in
-                        VStack(spacing: 0) {
-                            HStack(spacing: 12) {
-                                Image(systemName: setting.image)
-                                    .frame(width: 28)
-                                    .foregroundColor(Color.theme.other.primary)
-                                Text(setting.title)
-                                    .foregroundColor(Color.theme.text.main)
-                                    .font(.headline)
-                                Spacer()
-                                Image(systemName: "chevron.right")
-                                    .foregroundColor(Color.theme.other.primary)
-                                    .font(.title3)
+                        Link(destination: URL(string: setting.url)!, label: {
+                            VStack(spacing: 0) {
+                                HStack(spacing: 12) {
+                                    Image(systemName: setting.image)
+                                        .frame(width: 28)
+                                        .foregroundColor(Color.theme.other.primary)
+                                    Text(setting.title)
+                                        .foregroundColor(Color.theme.text.main)
+                                        .font(.headline)
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(Color.theme.other.primary)
+                                        .font(.title3)
+                                }
+                                .padding(.vertical, 20)
+                                
                             }
-                            .padding(.vertical, 20)
-                            
-                        }
+                        })
                     }
                 }
                 .padding(.horizontal, 16)
